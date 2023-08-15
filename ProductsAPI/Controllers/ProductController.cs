c89c392142de816337b8124b6422df8d3ac87b71
nulldeath <krawdev@gmail.com>
2023-08-15 16:46:30 -0300

    Routes implement

--- ProductsAPI/Controllers/ProductController.cs
+++ ProductsAPI/Controllers/ProductController.cs
@@ -1,13 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Data;
+using ProductsAPI.Models;
 
 namespace ProductsAPI.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        public async Task<IActionResult> GetProducts()
+
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
         {
-            
+            _context = context;
         }
 
+        [HttpGet]
+        [Route("/products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpPost]
+        [Route("/products")]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+
+        }
+
+        [HttpPost]
+        [Route("/products/{id}")]
+        public async Task<IActionResult> UpdateProduct(Guid id, Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [Route("/products/{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
     }
 }
